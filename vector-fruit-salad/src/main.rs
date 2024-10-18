2db137a99d724bccf241f96f0be40c50c71f3b42
klovertexarkana <97547512+klovertexarkana@users.noreply.github.com>
2024-10-18 17:34:59 +0000

    Pending changes exported from your codespace

--- vector-fruit-salad/src/main.rs
+++ vector-fruit-salad/src/main.rs
@@ -5,9 +5,17 @@ useful data structures in Rust. A vector is represented using the Vec<T> type.
 */
 
 use rand::seq::SliceRandom; // rand is a random number generation library in Rust
-use rand::thread_rng;
+use rand::{Rng, thread_rng};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+
+struct Args {
+    fruits: Vec<String>
+}
 
 fn main() {
+
     let mut fruit = vec![
         "Orange",
         "Fig",
@@ -17,10 +25,25 @@ fn main() {
         "Pear",
         "Peach",
     ];
+    let args = Args::parse();
+
+    if !args.fruits.is_empty() {
+        args.fruits.iter().for_each(|f| fruit.push(f));
+    }
 
     // Scramble (shuffle) the fruit
     let mut rng = thread_rng();
     fruit.shuffle(&mut rng);
+
+    let mut fruit_to_add = vec!["Banana", "Grapes", "Strawberry"];
+
+    let num_to_add = rng.gen_range(1..=fruit_to_add.len());
+
+    fruit_to_add.shuffle(&mut rng);
+
+    for i in 0..num_to_add {
+        fruit.push(fruit_to_add[i]);
+    }
 
     // Print out the fruit salad
     println!("Fruit Salad:");
@@ -30,5 +53,9 @@ fn main() {
         } else {
             println!("{}", item);
         }
+
     }
+
+    let random_fruit = fruit.choose(&mut rng).unwrap();   
+    println!("random fruit: {}", random_fruit);
 }
